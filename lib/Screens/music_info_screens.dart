5c9078cd88fa22586e1d23249f5b35370d5d56dd
yakuplacin <lightsuksesyon@gmail.com>
2021-11-19 21:34:41 +0300

    trying2

--- lib/Screens/music_info_screens.dart
+++ lib/Screens/music_info_screens.dart
@@ -206,57 +206,11 @@ class MusicInfoPage extends StatelessWidget {
           ],
         ),
       ),
-      bottomNavigationBar: _DemoBottomAppBar(),
+   
     );
   }
 }
 
-class _DemoBottomAppBar extends StatelessWidget {
-  const _DemoBottomAppBar({
-    this.fabLocation = FloatingActionButtonLocation.endDocked,
-    this.shape = const CircularNotchedRectangle(),
-  });
-
-  final FloatingActionButtonLocation fabLocation;
-  final NotchedShape? shape;
-
-  static final List<FloatingActionButtonLocation> centerLocations =
-      <FloatingActionButtonLocation>[
-    FloatingActionButtonLocation.centerDocked,
-    FloatingActionButtonLocation.centerFloat,
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return BottomAppBar(
-      shape: shape,
-      color: Colors.blue,
-      child: IconTheme(
-        data: IconThemeData(color: Theme.of(context).colorScheme.onPrimary),
-        child: Row(
-          children: <Widget>[
-            IconButton(
-              tooltip: 'Open navigation menu',
-              icon: const Icon(Icons.menu),
-              onPressed: () {},
-            ),
-            if (centerLocations.contains(fabLocation)) const Spacer(),
-            IconButton(
-              tooltip: 'Search',
-              icon: const Icon(Icons.search),
-              onPressed: () {},
-            ),
-            IconButton(
-              tooltip: 'Favorite',
-              icon: const Icon(Icons.favorite),
-              onPressed: () {},
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
 
 //import 'package:flutter/material.dart';
 //import 'package:term_project_beginning/infos/music_list.dart';
